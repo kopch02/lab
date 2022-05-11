be1b5d09ff453ef463464de6cf77bb0d5139bfbc
kopch <kopch08@gmail.com>
2022-05-11 15:17:43 +0700

    neiro_interpolycia+numpy

--- 4semestr/neironki/4lab/lab4.py
+++ 4semestr/neironki/4lab/lab4.py
@@ -1,47 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def sigmoid(x):
     return x * (1 - np.exp(x))
 
-ly=0.1
 
-x0=1
-x1=[1,2,3,4]
-x2=[2,3,4,5]
+ly = 0.1
 
-c1=np.array([1,0])
-c2=np.array([0,1])
+x = np.array([[1, 0], 
+            [1, 0.2], 
+            [1, 0.4], 
+            [1, 0.6], 
+            [1, 0.8], 
+            [1, 1]])
 
-wx=(-0.5-0.5)*np.random.random( size=(4, 2))+0.5
-wy=(-0.5-0.5)*np.random.random( size=(4, 2))+0.5
+c = np.array([[np.sin(0)], 
+                [np.sin(0.2)], 
+                [np.sin(0.4)], 
+                [np.sin(0.6)],
+                [np.sin(0.8)], 
+                [np.sin(1)]])
+                
+wx = (-0.5 - 0.5) * np.random.random(size=(2, 4)) + 0.5
+wy = (-0.5 - 0.5) * np.random.random(size=(5, 1)) + 0.5
+y0 = np.zeros((4, 5)) + 1
 
-y=[0,0,0,0]
-for epoh in range(100000+1):
-    for sample in range(2):
-        for neiron in range(4):#доделать
-            y[neiron]=sigmoid(x1[neiron]*x2[neiron]*wx[neiron])
-                        
-        z=sigmoid(np.dot(y,wy))
-        e1=c1[sample]-z
-        e2=c2[sample]-z
-        
-        delta1=e1*(z*(1-z))
-        delta2=e2*(z*(1-z))
-        
-        for i in range(4):
-            wy[0][i] += ly*y[i]*delta1
-            for x in range(2):
-                e=wy[0][i]*delta1*(y[i]*(1-y[i]))
-                wx[i][x]+=ly*e*x1[x]
-        
-        for i in range(4):
-            wy[1][i] += ly*y[i]*delta2
-            for x in range(2):
-                e=wy[1][i]*delta2*(y[i]*(1-y[i]))
-                wx[i][x]+=ly*e*x1[x]
-        
-        
-        
+for epoh in range(100000 + 1):
+    E = 0
+    y = sigmoid(np.dot(x, wx))
+    y_ = np.dot(y, y0)  #6x5
+    z = sigmoid(np.dot(y_, wy))  #6x2
+    e = c - z  #6x2
+    delta_out = e * (z * (1 - z))  #6x2
+    wy += np.transpose(ly * np.dot(np.transpose(delta_out), y_))  #5x2
+    delta_hid = y * (1 - y) * np.dot(delta_out, np.transpose(wy[1:]))  #6x4
+    #3x6    6x4
+    wx += ly * np.dot(np.transpose(x), delta_hid)  #3x4
 
-        
-        
+    E += e
+    if epoh % 5000 == 0:
+        print(E / 4)
+        print("z=", z)
+print()
+for i in range(6):
+    print(c[i][0], "   ", z[i][0])
