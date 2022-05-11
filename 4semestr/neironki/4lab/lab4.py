6e6facb2be1306d35f9f80f95864bb930c9a3694
kopch <kopch08@gmail.com>
2022-05-11 15:25:24 +0700

    neiro_interpolycia+numpy

--- 4semestr/neironki/4lab/lab4.py
+++ 4semestr/neironki/4lab/lab4.py
@@ -21,7 +21,6 @@ c = np.array([[np.sin(0)],
                 [np.sin(0.6)],
                 [np.sin(0.8)], 
                 [np.sin(1)]])
-                
 wx = (-0.5 - 0.5) * np.random.random(size=(2, 4)) + 0.5
 wy = (-0.5 - 0.5) * np.random.random(size=(5, 1)) + 0.5
 y0 = np.zeros((4, 5)) + 1
@@ -30,13 +29,13 @@ for epoh in range(100000 + 1):
     E = 0
     y = sigmoid(np.dot(x, wx))
     y_ = np.dot(y, y0)  #6x5
-    z = sigmoid(np.dot(y_, wy))  #6x2
-    e = c - z  #6x2
-    delta_out = e * (z * (1 - z))  #6x2
-    wy += np.transpose(ly * np.dot(np.transpose(delta_out), y_))  #5x2
+    z = sigmoid(np.dot(y_, wy))  #6x1
+    e = c - z  #6x1
+    delta_out = e * (z * (1 - z))  #6x1
+    wy += np.transpose(ly * np.dot(np.transpose(delta_out), y_))  #5x1
     delta_hid = y * (1 - y) * np.dot(delta_out, np.transpose(wy[1:]))  #6x4
-    #3x6    6x4
-    wx += ly * np.dot(np.transpose(x), delta_hid)  #3x4
+
+    wx += ly * np.dot(np.transpose(x), delta_hid)  #2x4
 
     E += e
     if epoh % 5000 == 0:
