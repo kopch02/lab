a2604c573d2a6f82ae2385551bd59b5d24632f87
kopch <kopch08@gmail.com>
2022-09-07 21:39:51 +0700

    new_paython_lab1_num5_dont+ready

--- 5semestr/python/lab1/lab1.py
+++ 5semestr/python/lab1/lab1.py
@@ -1,8 +1,9 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QCheckBox, QSpinBox
 from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 
 class num1(QWidget):
@@ -79,55 +80,45 @@ class num3(QWidget):
         self.initUI()
 
     def initUI(self):
-        
-        self.label_1 = QLabel(self)
-        self.label_1.setText("")
-        self.label_1.setGeometry(40,0,150,15)
+        funk={self.box1,self.box2,self.box3}
+        self.labels=[]
 
-        self.label_2 = QLabel(self)
-        self.label_2.setText("")
-        self.label_2.setGeometry(40,30,150,15)
+        y=0
+        n=1
 
-        self.label_3 = QLabel(self)
-        self.label_3.setText("")
-        self.label_3.setGeometry(40,60,150,15)
+        for i in funk:
+            self.check = QCheckBox(str(n), self)
+            self.check.move(10, y)
+            self.check.stateChanged.connect(i)
 
-        self.check_1 = QCheckBox('1', self)
-        self.check_1.move(10, 0)
-        self.check_1.stateChanged.connect(self.box1)
+            label = QLabel(self)
+            label.setText("")
+            label.setGeometry(40,y,150,15)
+            self.labels.append(label)
 
-        self.check_2 = QCheckBox('2', self)
-        self.check_2.move(10, 30)
-        self.check_2.stateChanged.connect(self.box2)
-
-        self.check_3 = QCheckBox('3', self)
-        self.check_3.move(10, 60)
-        self.check_3.stateChanged.connect(self.box3)
+            y+=30
+            n+=1
 
         self.show()
 
 
     def box1(self, state):
         if state == Qt.Checked:
-            self.label_1.setText("Первая строка ")
+            self.labels[0].setText("Первая строка ")
         else:
-            self.label_1.setText("")
+            self.labels[0].setText("")
     
     def box2(self, state):
-
-        print(self.sender().text())
-
-
         if state == Qt.Checked:
-            self.label_2.setText("Вторая строка ")
+            self.labels[1].setText("Вторая строка ")
         else:
-            self.label_2.setText("")
+            self.labels[1].setText("")
 
     def box3(self, state):
         if state == Qt.Checked:
-            self.label_3.setText("Третья строка ")
+            self.labels[2].setText("Третья строка ")
         else:
-            self.label_3.setText("")
+            self.labels[2].setText("")
 
 
 MORSE_CODE_DICT = { '1':'.----', '2':'..---', '3':'...--',
@@ -173,8 +164,81 @@ class num4(QWidget):
         self.label.setText(self.label.text()+" "+MORSE_CODE_DICT[self.sender().text()])
 
 
+class num5(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300,300,500,350)
+        self.setWindowTitle("меню")
+
+        paths=['5semestr\python\lab1\\img\\burger.jpeg','5semestr\python\lab1\\img\\napitok.jpg','5semestr\python\lab1\\img\\sup.jpg']
+        y=20
+        for i in paths:
+            photo_label = QLabel(self)
+            photo = QPixmap(i)
+            photo=photo.scaled(135,90)
+            photo_label.setPixmap(photo)
+            photo_label.move(10,y)
+            y+=110
+
+        y=65
+        phrase=("добавить бургер","добавить напиток","добавить суп")
+        price=("390₽","90₽","190₽")
+        #funk={self.box1,self.box2,self.box3}
+        self.labels=[]
+
+        for i in range(3):
+            self.check = QCheckBox(phrase[i], self)
+            self.check.move(160, y)
+            self.check.stateChanged.connect(self.box1)
+            self.check.setStyleSheet('''
+                QCheckBox::indicator:unchecked {
+                    image: url(5semestr/python/lab1/img/off.png);
+                }
+                QCheckBox::indicator:checked {
+                    image: url(5semestr/python/lab1/img/on.png);
+                }
+            ''')
+
+            label = QLabel(self)
+            label.setText(price[i])
+            label.setGeometry(160,y-30,50,15)
+            self.labels.append(label)
+
+            y+=110
+
+        self.widget_1 = QSpinBox(self)
+        self.widget_1.setMinimum(1)
+        self.widget_1.setSingleStep(1)
+        self.widget_1.move(330,50)
+        self.widget_1.hide()
+
+        self.widget_2 = QSpinBox(self)
+        self.widget_2.setMinimum(1)
+        self.widget_2.setSingleStep(1)
+        self.widget_2.move(330,50)
+        self.widget_2.hide()
+
+        self.widget_3 = QSpinBox(self)
+        self.widget_3.setMinimum(1)
+        self.widget_3.setSingleStep(1)
+        self.widget_3.move(330,50)
+        self.widget_3.hide()
+
+        self.show()
+
+    def box1(self,state):
+        if state == Qt.Checked:
+            self.widget_1.show()
+        else:
+            self.widget_1.hide()
+
+    
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = num1()
+    ex = num5()
     ex.show()
     sys.exit(app.exec())
