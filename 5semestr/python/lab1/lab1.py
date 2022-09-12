7525af3d9e5f23bd63d2ab52c8420fc8601fb581
kopch <kopch08@gmail.com>
2022-09-13 00:40:04 +0700

    1_lab

--- 5semestr/python/lab1/lab1.py
+++ 5semestr/python/lab1/lab1.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QCheckBox, QSpinBox
 from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QFont
 
 
 class num1(QWidget):
@@ -218,19 +218,17 @@ class num5(QWidget):
         y = 65
         phrase = ("добавить бургер", "добавить напиток", "добавить суп")
         price = ("390₽", "90₽", "190₽")
-        self.funk = [self.box1, self.box2, self.box3]
-        self.funk_spin = [self.spin_1, self.spin_2, self.spin_3]
+        self.price2 = (390, 90, 190)
         self.labels = []
         self.spins = []
+        self.sums = [0, 0, 0]
         self.sum = 0
-        self.s_1 = 0
-        self.s_2 = 0
-        self.s_3 = 0
 
         for i in range(3):
             self.check = QCheckBox(phrase[i], self)
             self.check.move(160, y)
-            self.check.stateChanged.connect(self.funk[i])
+            self.check.id = i
+            self.check.stateChanged.connect(self.box)
             self.check.setStyleSheet('''
                 QCheckBox::indicator:unchecked {
                     image: url(5semestr/python/lab1/img/off.png);
@@ -250,15 +248,16 @@ class num5(QWidget):
             self.spin.setSingleStep(1)
             self.spin.move(330, y)
             self.spin.hide()
-            self.spin.valueChanged.connect(self.funk_spin[i])
+            self.spin.valueChanged.connect(self.spin_update)
+            self.spin.id = i
             self.spins.append(self.spin)
 
             y += 110
 
         self.button = QPushButton(self)
         self.button.move(410, 300)
-        self.button.setText("Вычислить")
-        self.button.clicked.connect(self.res)
+        self.button.setText("Сделать заказ")
+        self.button.clicked.connect(self.summ)
 
         self.label = QLabel(self)
         self.label.setText("0₽")
@@ -266,68 +265,111 @@ class num5(QWidget):
 
         self.show()
 
-    def box1(self, state):
+    def box(self, state):
         if state == Qt.Checked:
-            self.spins[0].show()
-            self.spins[0].setValue(1)
-            self.s_1 = self.spins[0].value() * 390
-            self.update()
+            self.spins[self.sender().id].show()
+            self.spins[self.sender().id].setValue(1)
 
         else:
-            self.spins[0].hide()
-            self.spins[0].setValue(0)
+            self.spins[self.sender().id].hide()
+            self.spins[self.sender().id].setValue(0)
             self.update()
 
-
-    def box2(self, state):
-        if state == Qt.Checked:
-            self.spins[1].show()
-            self.spins[1].setValue(1)
-            self.s_2 = self.spins[1].value() * 90
-            self.update()
-
-        else:
-            self.spins[1].hide()
-            self.spins[1].setValue(0)
-            self.update()
-
-
-    def box3(self, state):
-        if state == Qt.Checked:
-            self.spins[2].show()
-            self.spins[2].setValue(1)
-            self.s_3 = self.spins[2].value() * 190
-            self.update()
-
-        else:
-            self.spins[2].hide()
-            self.spins[2].setValue(0)
-            self.update()
-
-
-    def spin_1(self, value):
-        self.s_1 = value * 390
-        self.update()
-
-    def spin_2(self, value):
-        self.s_2 = value * 90
-        self.update()
-
-    def spin_3(self, value):
-        self.s_3 = value * 190
-        self.update()
-
-    def res(self):
-        self.sum = self.s_1 + self.s_2 + self.s_3
+    def spin_update(self, value):
+        self.sums[self.sender().id] = value * self.price2[self.sender().id]
         self.update()
 
     def update(self):
-        self.sum = self.s_1 + self.s_2 + self.s_3
+        for i in self.sums:
+            self.sum += i
         self.label.setText(str(self.sum) + "₽")
+        self.sum = 0
+
+    def summ(self):
+        self.check = check(price=self.sums)
+        self.check.show()
+
+
+class check(QWidget):
+
+    def __init__(self, price, *args):
+        super().__init__()
+        self.initUI(args, price=price)
+
+    def initUI(self, args, price):
+        self.setGeometry(400, 330, 180, 200)
+        self.setWindowTitle('чек')
+        self.label = QLabel(self)
+        self.label.setGeometry(0, 0, 200, 70)
+        self.final_text = ""
+        if int(price[0] / 390) != 0:
+            self.burger = f"Бургер x{int(price[0]/390)}\t\t{price[0]}\n"
+            self.final_text += self.burger
+        if int(price[1] / 90) != 0:
+            self.napitok = f"Напиток x{int(price[1]/90)}\t\t{price[1]}\n"
+            self.final_text += self.napitok
+        if int(price[2] / 190) != 0:
+            self.sup = f"Суп x{int(price[2]/190)}\t\t\t{price[2]}\n"
+            self.final_text += self.sup
+        self.final_text += f"\n\tсумма:\t\t{str(price[0]+price[1]+price[2])}"
+        self.label.setText(self.final_text)
+
+        self.btn = QPushButton(self)
+        self.btn.setText("OK")
+        self.btn.setGeometry(75, 170, 30, 30)
+        self.btn.clicked.connect(self.hide)
+
+
+class num6(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 485, 550)
+        self.setWindowTitle("калькулятор")
+        self.btns = [
+            "C", "DEL", "*", "=", "1", "2", "3", "/", "4", "5", "6", "+", "7",
+            "8", "9", "-", "+/-", "0", "%", "X^2"
+        ]
+        x = 10
+        y = 140
+        for bt in self.btns:
+            self.b = QPushButton(self)
+            self.b.setText(bt)
+            self.b.setGeometry(x, y, 115, 79)
+            self.b.clicked.connect(self.logicalc)
+            x += 117
+            if x > 400:
+                x = 10
+                y += 81
+        self.formula = "0"
+        self.lbl = QLabel(self,text=self.formula)
+        self.lbl.setGeometry(11,50,480,100)
+        self.lbl.setFont(QFont("Times", 16, QFont.Bold))
+
+    def summ(self):
+        print(self.sender.text())
+
+    def logicalc(self):
+        if self.sender().text() == "C":
+            self.formula = ""
+        elif self.sender().text() == "DEL":
+            self.formula = self.formula[0:-1]
+        elif self.sender().text() == "X^2":
+            self.formula = str((eval(self.formula))**2)
+        elif self.sender().text() == "=":
+            self.formula = str(eval(self.formula))
+        else:
+            if self.formula == "0":
+                self.formula = ""
+            self.formula += self.sender().text()
+        self.lbl.setText(self.formula)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = num5()
+    ex = num6()
     ex.show()
     sys.exit(app.exec())
