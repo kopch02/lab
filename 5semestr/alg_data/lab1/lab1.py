9e0000b774ef80886b2792d6f7e95ef6c780c329
kopch <kopch08@gmail.com>
2022-09-17 18:18:33 +0700

    psevdo

--- 5semestr/alg_data/lab1/lab1.py
+++ 5semestr/alg_data/lab1/lab1.py
@@ -1,6 +1,6 @@
 "вариант 5"
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import timeit
 
 
@@ -8,22 +8,30 @@ def num1():
     def foo(nums): # nums - список
         for x in nums:
             if x % 2 == 0:
+                print("!!")
                 return True
         else:
             return False
 
+    li=[]
+    time = timeit.Timer("foo(li)", globals=locals())
 
+    #timeit.timeit(f"foo({list(range(1,i,2))})", number=100, globals=locals())
     plt_x = []
     plt_y=[]
 
-    for i in range(1000,10001,1000):
+
+    for i in range(1000,100001,100):
+        li=list(range(1,i,2))
         plt_x.append(i)
-        plt_y.append(timeit.timeit(f"foo({i})", number=100, globals=locals()))
+        t=time.timeit(number=1000)
+        plt_y.append(t)
 
     print("функция возвращает True если в списке встречается чётное число и False если не одного чётного нет")
     print("вычислительная сложность O(2n)")
 
     plt.plot(plt_x, plt_y)
     plt.show()
+
 
 num1()
