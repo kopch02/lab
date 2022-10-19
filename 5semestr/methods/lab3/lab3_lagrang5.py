cdcf966f1e97653299635e88a746dfc80b87e856
kopch <kopch08@gmail.com>
2022-10-19 10:11:55 +0700

    lagrange_5

--- 5semestr/methods/lab3/lab3_lagrang5.py
+++ 5semestr/methods/lab3/lab3_lagrang5.py
@@ -2,13 +2,15 @@ import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
 
-a = np.arange(-2, 1.1, 0.1)
+a = np.arange(-3, 2.1, 0.1)
 y = np.log10(a**2 + a + 1)
 
-res=y[0] * (a - a[10]) * (a - a[20]) * (a - a[30]) / ((a[0] - a[10]) * (a[0] - a[20]) * (a[0] - a[30])) +\
-y[10] * (a - a[0]) * (a - a[20]) * (a - a[30]) / ((a[10] - a[0]) * (a[10] - a[20]) * (a[10] - a[30])) +\
-y[20] * (a - a[10]) * (a - a[0]) * (a - a[30]) / ((a[20] - a[10]) * (a[20] - a[0]) * (a[20] - a[30])) +\
-y[30] * (a - a[10]) * (a - a[20]) * (a - a[0]) / ((a[30] - a[10]) * (a[30] - a[20]) * (a[30] - a[0]))
+res=y[0] * (a - a[10]) * (a - a[20]) * (a - a[30]) * (a - a[40]) * (a - a[50])/ ((a[0] - a[10]) * (a[0] - a[20]) * (a[0] - a[30]) * (a[0] - a[40]) * (a[0] - a[50])) +\
+y[10] * (a - a[0]) * (a - a[20]) * (a - a[30]) * (a - a[40]) * (a - a[50]) / ((a[10] - a[0]) * (a[10] - a[20]) * (a[10] - a[30]) * (a[10] - a[40]) * (a[10] - a[50])) +\
+y[20] * (a - a[10]) * (a - a[0]) * (a - a[30]) * (a - a[40]) * (a - a[50]) / ((a[20] - a[10]) * (a[20] - a[0]) * (a[20] - a[30]) * (a[20] - a[40]) * (a[20] - a[50])) +\
+y[30] * (a - a[10]) * (a - a[20]) * (a - a[0]) * (a - a[40]) * (a - a[50]) / ((a[30] - a[10]) * (a[30] - a[20]) * (a[30] - a[0]) * (a[30] - a[40]) * (a[30] - a[50])) +\
+y[40] * (a - a[10]) * (a - a[20]) * (a - a[30]) * (a - a[0]) * (a - a[50]) / ((a[40] - a[10]) * (a[40] - a[20]) * (a[40] - a[30]) * (a[40] - a[0]) * (a[40] - a[50])) +\
+y[50] * (a - a[10]) * (a - a[20]) * (a - a[30]) * (a - a[40]) * (a - a[0]) / ((a[50] - a[10]) * (a[50] - a[20]) * (a[50] - a[30]) * (a[50] - a[40]) * (a[50] - a[0])) 
 
 print(res)
 
@@ -16,37 +18,22 @@ r_pract = abs(y - res)
 print(r_pract)
 
 
-def diff4(i):
-    x = sp.symbols('x')
-    func = sp.log(x**2 + x + 1)
-    yprime = sp.diff(func, x, 4)
-    yprime = yprime.subs({x: i})
-    return yprime.evalf()
 
-r_teor = []
-
-for x in a:
-    r_teor.append(round((abs(diff4(x) / 24 * (x-a[0]) * (x - a[10]) * (x - a[20]) * (x - a[30]))), 9))
-
-print(r_teor)
-r_teor=np.array(r_teor)
-
-print("res \t\t\t r_pract \t\t\t r_teor")
+print("x \t\t\t res \t\t\t r_pract")
 
 for i in range(len(a)):
-    print(res[i],"\t",r_pract[i],"\t",r_teor[i])
+    print(a[i],"\t",res[i],"\t",r_pract[i])
 
 figure = plt.figure()
 
 plt.subplot(1, 2, 1)
 plt.plot(a,y)
 plt.plot(a,res)
-plt.legend(["y","L3"])
+plt.legend(["y","L5"])
 
 plt.subplot(1, 2, 2)
 plt.plot(r_pract)
-plt.plot(r_teor)
-plt.legend(["r_pract","r_teor"])
+plt.legend(["r_pract"])
 
 plt.subplots_adjust(top=0.9, bottom=0.2, wspace=0.25)
 plt.show()
