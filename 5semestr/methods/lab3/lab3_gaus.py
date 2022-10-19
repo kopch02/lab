9cc40fabc15e176005a1a88fbb88ec8fc6de40af
kopch <kopch08@gmail.com>
2022-10-19 10:28:35 +0700

    gaus2

--- 5semestr/methods/lab3/lab3_gaus.py
+++ 5semestr/methods/lab3/lab3_gaus.py
@@ -5,50 +5,34 @@ import matplotlib.pyplot as plt
 a = np.arange(-2, 0.1, 0.1)
 y = np.log10(a**2 + a + 1)
 
+del_y = [y[10] - y[0], y[20] - y[10]]
+del2_y = del_y[1] - del_y[0]
+
 t=(a-a[0])/(a[10]-a[0])
 
-res=y[0] * (a - a[10]) * (a - a[20]) * (a - a[30]) / ((a[0] - a[10]) * (a[0] - a[20]) * (a[0] - a[30])) +\
-y[10] * (a - a[0]) * (a - a[20]) * (a - a[30]) / ((a[10] - a[0]) * (a[10] - a[20]) * (a[10] - a[30])) +\
-y[20] * (a - a[10]) * (a - a[0]) * (a - a[30]) / ((a[20] - a[10]) * (a[20] - a[0]) * (a[20] - a[30])) +\
-y[30] * (a - a[10]) * (a - a[20]) * (a - a[0]) / ((a[30] - a[10]) * (a[30] - a[20]) * (a[30] - a[0]))
+print(del_y)
+print(del2_y)
+print(t)
 
-print(res)
+res = y[0] + del_y[0] * t + del2_y * t * (t - 1)/2
 
 r_pract = abs(y - res)
-print(r_pract)
 
-
-def diff4(i):
-    x = sp.symbols('x')
-    func = sp.log(x**2 + x + 1)
-    yprime = sp.diff(func, x, 4)
-    yprime = yprime.subs({x: i})
-    return yprime.evalf()
-
-r_teor = []
-
-for x in a:
-    r_teor.append(round((abs(diff4(x) / 24 * (x-a[0]) * (x - a[10]) * (x - a[20]) * (x - a[30]))), 9))
-
-print(r_teor)
-r_teor=np.array(r_teor)
-
-print("res \t\t\t r_pract \t\t\t r_teor")
+print("x \t\t\t\t res \t\t\t\t r_pract")
 
 for i in range(len(a)):
-    print(res[i],"\t",r_pract[i],"\t",r_teor[i])
+    print(a[i],"\t\t",res[i],"\t\t",r_pract[i])
 
 figure = plt.figure()
 
 plt.subplot(1, 2, 1)
 plt.plot(a,y)
 plt.plot(a,res)
-plt.legend(["y","L3"])
+plt.legend(["y","gauss2"])
 
 plt.subplot(1, 2, 2)
 plt.plot(r_pract)
-plt.plot(r_teor)
-plt.legend(["r_pract","r_teor"])
+plt.legend(["r_pract"])
 
 plt.subplots_adjust(top=0.9, bottom=0.2, wspace=0.25)
 plt.show()
